d3635e73bfe889902474749c5d630c8ebed64979
Jerome Wu <jeromewus@gmail.com>
2019-10-30 17:57:55 +0800

    Add logging feature

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 require('regenerator-runtime/runtime');
+const { logging, setLogging } = require('./utils/log');
 const createWorker = require('./createWorker');
 
 module.exports = {
+  logging,
+  setLogging,
   createWorker,
 };
