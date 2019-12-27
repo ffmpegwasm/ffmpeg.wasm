d3860259cd5a466635ab7fe8de55ba379171d2d5
jeromewu <jeromewus@gmail.com>
2019-12-27 09:03:54 +0000

    Add electron option when detecting environment

--- src/utils/getEnvironment.js
+++ src/utils/getEnvironment.js
@@ -1,10 +1,21 @@
+const isElectron = require('is-electron');
+
 module.exports = (key) => {
-  const env = {
-    type: (typeof window !== 'undefined') && (typeof window.document !== 'undefined') ? 'browser' : 'node',
-  };
+  const env = {};
+
+  if (isElectron()) {
+    env.type = 'electron';
+  } else if (typeof window === 'object') {
+    env.type = 'browser';
+  } else if (typeof importScripts === 'function') {
+    env.type = 'webworker';
+  } else if (typeof process === 'object' && typeof require === 'function') {
+    env.type = 'node';
+  }
 
   if (typeof key === 'undefined') {
     return env;
   }
+
   return env[key];
 };
