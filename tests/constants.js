235bf3dda07d5089e7859aaafa2ae6d3ef161b66
Jerome Wu <jeromewus@gmail.com>
2020-04-29 09:27:21 +0800

    Fix worker script origin issue

--- tests/constants.js
+++ tests/constants.js
@@ -2,7 +2,7 @@ const TIMEOUT = 60000;
 const BASE_URL = 'http://localhost:3000/tests/assets';
 const IS_BROWSER = typeof window !== 'undefined' && typeof window.document !== 'undefined';
 const OPTIONS = {
-  corePath: '../node_modules/@ffmpeg/core/ffmpeg-core.js',
+  corePath: 'http://localhost:3000/node_modules/@ffmpeg/core/ffmpeg-core.js',
 };
 
 if (typeof module !== 'undefined') {
