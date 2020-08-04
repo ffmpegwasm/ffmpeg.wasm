5e69f82b6a75ebb8867e8c73b896f1189bc91d20
Rahul Tarak <rahultarak12345@gmail.com>
2020-08-04 14:31:52 +0530

    🛣 Local Load Path

--- src/browser/defaultOptions.js
+++ src/browser/defaultOptions.js
@@ -1,13 +1,18 @@
-const resolveURL = require('resolve-url');
-const { dependencies } = require('../../package.json');
-const defaultOptions = require('../constants/defaultOptions');
+const resolveURL = require("resolve-url");
+const { dependencies } = require("../../package.json");
+const defaultOptions = require("../constants/defaultOptions");
 
 /*
  * Default options for browser worker
  */
 module.exports = {
   ...defaultOptions,
-  corePath: (typeof process !== 'undefined' && process.env.FFMPEG_ENV === 'development')
-    ? resolveURL('/node_modules/@ffmpeg/core/ffmpeg-core.js')
-    : `https://unpkg.com/@ffmpeg/core@v${dependencies['@ffmpeg/core'].substring(1)}/ffmpeg-core.js`,
+  corePath:
+    typeof process !== "undefined" && process.env.FFMPEG_ENV === "development"
+      ? resolveURL("/node_modules/@ffmpeg/core/ffmpeg-core.js")
+      : process.env.PUBLIC_URL
+      ? `${process.env.PUBLIC_}/ffmpeg-core.js`
+      : `https://unpkg.com/@ffmpeg/core@v${dependencies[
+          "@ffmpeg/core"
+        ].substring(1)}/ffmpeg-core.js`,
 };
