6d318945ddf60d53740e3ef0253be1a1e386955c
Rahul Tarak <rahultarak12345@gmail.com>
2020-08-04 14:32:00 +0530

    10 Second Timeout

--- src/browser/getModule.js
+++ src/browser/getModule.js
@@ -1,28 +1,40 @@
-const { log } = require('../utils/log');
+const { log } = require("../utils/log");
 
 module.exports = async ({ corePath }) => {
-  if (typeof window.Module === 'undefined') {
-    log('info', 'fetch ffmpeg-core.worker.js script');
-    const workerBlob = await (await fetch(corePath.replace('ffmpeg-core.js', 'ffmpeg-core.worker.js'))).blob();
+  if (typeof window.Module === "undefined") {
+    log("info", "fetch ffmpeg-core.worker.js script");
+    const workerBlob = await (
+      await fetch(corePath.replace("ffmpeg-core.js", "ffmpeg-core.worker.js"))
+    ).blob();
     window.FFMPEG_CORE_WORKER_SCRIPT = URL.createObjectURL(workerBlob);
-    log('info', `worker object URL=${window.FFMPEG_CORE_WORKER_SCRIPT}`);
-    log('info', `download ffmpeg-core script (~25 MB) from ${corePath}`);
-    return new Promise((resolve) => {
-      const script = document.createElement('script');
+    log("info", `worker object URL=${window.FFMPEG_CORE_WORKER_SCRIPT}`);
+    log("info", `download ffmpeg-core script (~25 MB) from ${corePath}`);
+    const timeoutPromise = new Promise((_, reject) => {
+      setTimeout(() => {
+        reject(
+          new Error(
+            `Loading FFmpeg Failed! This is because it either timed out or we do not support your browser yet. ${window.navigator.userAgent}`
+          )
+        );
+      }, 10000);
+    });
+    const loadFFmpeg = new Promise((resolve) => {
+      const script = document.createElement("script");
       const eventHandler = () => {
-        script.removeEventListener('load', eventHandler);
-        log('info', 'initialize ffmpeg-core');
+        script.removeEventListener("load", eventHandler);
+        log("info", "initialize ffmpeg-core");
         window.Module.onRuntimeInitialized = () => {
-          log('info', 'ffmpeg-core initialized');
+          log("info", "ffmpeg-core initialized");
           resolve(window.Module);
         };
       };
       script.src = corePath;
-      script.type = 'text/javascript';
-      script.addEventListener('load', eventHandler);
-      document.getElementsByTagName('head')[0].appendChild(script);
+      script.type = "text/javascript";
+      script.addEventListener("load", eventHandler);
+      document.getElementsByTagName("head")[0].appendChild(script);
     });
+    return Promise.race([loadFFmpeg, timeoutPromise]);
   }
-  log('info', 'ffmpeg-core is loaded already');
+  log("info", "ffmpeg-core is loaded already");
   return Promise.resolve(window.Module);
 };
