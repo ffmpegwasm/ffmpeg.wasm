c398540992e0add04a9ecbba04a29971f5b0b0ed
Jerome Wu <jeromewus@gmail.com>
2020-11-03 17:41:42 +0800

    Remove `v` in version and add locateFile func to fix worker script issue

--- src/browser/defaultOptions.js
+++ src/browser/defaultOptions.js
@@ -7,5 +7,5 @@ const { devDependencies } = require('../../package.json');
 module.exports = {
   corePath: (typeof process !== 'undefined' && process.env.FFMPEG_ENV === 'development')
     ? resolveURL('/node_modules/@ffmpeg/core/dist/ffmpeg-core.js')
-    : `https://unpkg.com/@ffmpeg/core@v${devDependencies['@ffmpeg/core'].substring(1)}/dist/ffmpeg-core.js`,
+    : `https://unpkg.com/@ffmpeg/core@${devDependencies['@ffmpeg/core'].substring(1)}/dist/ffmpeg-core.js`,
 };
