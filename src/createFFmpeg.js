4548e4cb79dad4d6ff235f6a956386177fa37c8b
seminelee <747191197@qq.com>
2021-07-28 20:44:04 +0800

    U createFFmpeg.js

--- src/createFFmpeg.js
+++ src/createFFmpeg.js
@@ -88,7 +88,8 @@ module.exports = (_options = {}) => {
           return prefix + path;
         },
       });
-      ffmpeg = Core.cwrap('proxy_main', 'number', ['number', 'number']);
+      const mainName = options.pthread === 0 ? '_main' : 'proxy_main';
+      ffmpeg = Core.cwrap(mainName, 'number', ['number', 'number']);
       log('info', 'ffmpeg-core loaded');
     } else {
       throw Error('ffmpeg.wasm was loaded, you should not load it again, use ffmpeg.isLoaded() to check next time.');
