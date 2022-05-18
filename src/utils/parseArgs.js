3ab076d5b49dacb4bcd7422778b10552cce34980
cs8425 <cs8425@gmail.com>
2022-05-18 15:35:43 +0800

    use lengthBytesUTF8() to compute the exact bytes of string

--- src/utils/parseArgs.js
+++ src/utils/parseArgs.js
@@ -1,7 +1,7 @@
 module.exports = (Core, args) => {
   const argsPtr = Core._malloc(args.length * Uint32Array.BYTES_PER_ELEMENT);
   args.forEach((s, idx) => {
-    const sz = s.length*4 + 1;
+    const sz = Core.lengthBytesUTF8(s) + 1;
     const buf = Core._malloc(sz);
     Core.stringToUTF8(s, buf, sz);
     Core.setValue(argsPtr + (Uint32Array.BYTES_PER_ELEMENT * idx), buf, 'i32');
