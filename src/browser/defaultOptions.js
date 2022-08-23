d8240455923f6bda939a1a3658ebfdea5e8bf93f
Jerome Wu <jeromewus@gmail.com>
2022-08-23 08:54:58 +0800

    Fix vite issue

--- src/browser/defaultOptions.js
+++ src/browser/defaultOptions.js
@@ -1,10 +1,10 @@
-const { devDependencies } = require('../../package.json');
+import pkg from '../../package.json';
 
 /*
  * Default options for browser environment
  */
 const corePath = typeof process !== 'undefined' && process.env.NODE_ENV === 'development'
   ? new URL('/node_modules/@ffmpeg/core/dist/ffmpeg-core.js', import.meta.url).href
-  : `https://unpkg.com/@ffmpeg/core@${devDependencies['@ffmpeg/core'].substring(1)}/dist/ffmpeg-core.js`;
+  : `https://unpkg.com/@ffmpeg/core@${pkg.devDependencies['@ffmpeg/core'].substring(1)}/dist/ffmpeg-core.js`;
 
 export default { corePath };
