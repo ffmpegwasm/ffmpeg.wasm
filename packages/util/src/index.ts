560f42cf0f121c89e5dad7250217e85a45303262
Jerome Wu <jeromewus@gmail.com>
2022-09-23 17:11:48 +0800

    Add util and rewrite examples (WIP)

---
+++ packages/util/src/index.ts
@@ -0,0 +1,107 @@
+export const readFromBlobOrFile = (blob: Blob | File): Promise<Uint8Array> =>
+  new Promise((resolve, reject) => {
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+      const { result } = fileReader;
+      if (result instanceof ArrayBuffer) {
+        resolve(new Uint8Array(result));
+      } else {
+        resolve(new Uint8Array());
+      }
+    };
+    fileReader.onerror = (event) => {
+      reject(
+        Error(
+          `File could not be read! Code=${event?.target?.error?.code || -1}`
+        )
+      );
+    };
+    fileReader.readAsArrayBuffer(blob);
+  });
+
+/**
+ * An util function to fetch data from url string, base64, URL, File or Blob format.
+ *
+ * Examples:
+ * ```ts
+ * // URL
+ * await fetchFile("http://localhost:3000/video.mp4");
+ * // base64
+ * await fetchFile("data:<type>;base64,wL2dvYWwgbW9yZ...");
+ * // URL
+ * await fetchFile(new URL("video.mp4", import.meta.url));
+ * // File
+ * fileInput.addEventListener('change', (e) => {
+ *   await fetchFile(e.target.files[0]);
+ * });
+ * // Blob
+ * const blob = new Blob(...);
+ * await fetchFile(blob);
+ * ```
+ */
+export const fetchFile = async (
+  file?: string | File | Blob
+): Promise<Uint8Array> => {
+  let data: ArrayBuffer | number[];
+
+  if (typeof file === "string") {
+    /* From base64 format */
+    if (/data:_data\/([a-zA-Z]*);base64,([^"]*)/.test(file)) {
+      data = atob(file.split(",")[1])
+        .split("")
+        .map((c) => c.charCodeAt(0));
+      /* From remote server/URL */
+    } else {
+      data = await (await fetch(file)).arrayBuffer();
+    }
+  } else if (file instanceof URL) {
+    data = await (await fetch(file)).arrayBuffer();
+  } else if (file instanceof File || file instanceof Blob) {
+    data = await readFromBlobOrFile(file);
+  } else {
+    return new Uint8Array();
+  }
+
+  return new Uint8Array(data);
+};
+
+/**
+ * importScript dynamically import a script, useful when you
+ * want to use different versions of ffmpeg.wasm based on environment.
+ *
+ * Example:
+ *
+ * ```ts
+ * await importScript("http://localhost:3000/ffmpeg.js");
+ * ```
+ */
+export const importScript = async (url: string): Promise<void> =>
+  new Promise((resolve) => {
+    const script = document.createElement("script");
+    const eventHandler = () => {
+      script.removeEventListener("load", eventHandler);
+      resolve();
+    };
+    script.src = url;
+    script.type = "text/javascript";
+    script.addEventListener("load", eventHandler);
+    document.getElementsByTagName("head")[0].appendChild(script);
+  });
+
+/**
+ * toBlobURL fetches data from an URL and return a blob URL.
+ *
+ * Example:
+ *
+ * ```ts
+ * await toBlobURL("http://localhost:3000/ffmpeg.js", "text/javascript");
+ * ```
+ */
+export const toBlobURL = async (
+  url: string,
+  mimeType: string
+): Promise<string> => {
+  const buf = await (await fetch(url)).arrayBuffer();
+  const blob = new Blob([buf], { type: mimeType });
+  return URL.createObjectURL(blob);
+};
