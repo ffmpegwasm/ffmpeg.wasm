1ddf1df6f191d66198ff5a4f7e4d5d9d26df7a7a
Jerome Wu <jeromewus@gmail.com>
2022-10-06 20:52:39 +0800

    Update website and @ffmpeg/util

---
+++ apps/website/src/components/Playground/const.ts
@@ -0,0 +1,12 @@
+export const CORE_SIZE = {
+  "https://unpkg.com/@ffmpeg/core@0.12.0-alpha.2/dist/umd/ffmpeg-core.js": 111646,
+  "https://unpkg.com/@ffmpeg/core@0.12.0-alpha.2/dist/umd/ffmpeg-core.wasm": 31967534,
+  "https://unpkg.com/@ffmpeg/core-mt@0.12.0-alpha.2/dist/umd/ffmpeg-core.js": 130002,
+  "https://unpkg.com/@ffmpeg/core-mt@0.12.0-alpha.2/dist/umd/ffmpeg-core.wasm": 32441947,
+  "https://unpkg.com/@ffmpeg/core-mt@0.12.0-alpha.2/dist/umd/ffmpeg-core.worker.js": 2978,
+};
+
+export const SAMPLE_FILES = {
+  "video.avi":
+    "https://raw.githubusercontent.com/ffmpegwasm/testdata/master/video-3s.avi",
+};
