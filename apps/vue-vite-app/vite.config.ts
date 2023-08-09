716a229ecb15a2a88e7f36a92c25837a2dfab753
Jerome Wu <jeromewus@gmail.com>
2023-08-09 17:18:28 +0800

    Update vue-vite-app with mt version

--- apps/vue-vite-app/vite.config.ts
+++ apps/vue-vite-app/vite.config.ts
@@ -11,5 +11,11 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp'
+    }
   }
 })
