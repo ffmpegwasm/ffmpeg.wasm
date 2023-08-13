030b03fab4af7ee8a2b08ca05b5a6101c1f0afc4
Jerome Wu <jeromewus@gmail.com>
2023-08-13 21:02:25 +0800

    Add optimizeDeps exclude

--- apps/react-vite-app/vite.config.ts
+++ apps/react-vite-app/vite.config.ts
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    exclude: ["@ffmpeg/ffmpeg", "@ffmpeg/util"],
+  },
   server: {
     headers: {
       "Cross-Origin-Opener-Policy": "same-origin",
--- apps/vue-vite-app/vite.config.ts
+++ apps/vue-vite-app/vite.config.ts
@@ -12,6 +12,9 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
+  optimizeDeps: {
+    exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util']
+  },
   server: {
     headers: {
       'Cross-Origin-Opener-Policy': 'same-origin',
