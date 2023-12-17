ea373ad6879239175f2ccba4e6f943ddcc2ac339
jeromewu <jeromewus@gmail.com>
2023-12-17 13:12:50 +0800

    Release 0.12.8 | 0.12.5

--- apps/website/src/components/Playground/const.ts
+++ apps/website/src/components/Playground/const.ts
@@ -1,13 +1,13 @@
-export const CORE_VERSION = "0.12.4";
+export const CORE_VERSION = "0.12.5";
 
 export const CORE_URL = `https://unpkg.com/@ffmpeg/core@${CORE_VERSION}/dist/umd/ffmpeg-core.js`;
 export const CORE_MT_URL = `https://unpkg.com/@ffmpeg/core-mt@${CORE_VERSION}/dist/umd/ffmpeg-core.js`;
 
 export const CORE_SIZE = {
   [`https://unpkg.com/@ffmpeg/core@${CORE_VERSION}/dist/umd/ffmpeg-core.js`]: 114673,
-  [`https://unpkg.com/@ffmpeg/core@${CORE_VERSION}/dist/umd/ffmpeg-core.wasm`]: 31739621,
+  [`https://unpkg.com/@ffmpeg/core@${CORE_VERSION}/dist/umd/ffmpeg-core.wasm`]: 31742481,
   [`https://unpkg.com/@ffmpeg/core-mt@${CORE_VERSION}/dist/umd/ffmpeg-core.js`]: 132680,
-  [`https://unpkg.com/@ffmpeg/core-mt@${CORE_VERSION}/dist/umd/ffmpeg-core.wasm`]: 32220432,
+  [`https://unpkg.com/@ffmpeg/core-mt@${CORE_VERSION}/dist/umd/ffmpeg-core.wasm`]: 32223257,
   [`https://unpkg.com/@ffmpeg/core-mt@${CORE_VERSION}/dist/umd/ffmpeg-core.worker.js`]: 2915,
 };
 
--- scripts/download-assets.js
+++ scripts/download-assets.js
@@ -6,8 +6,8 @@ const ROOT = "public/assets";
 
 const FFMPEG_VERSION = "0.12.7";
 const UTIL_VERSION = "0.12.0";
-const CORE_VERSION = "0.12.4";
-const CORE_MT_VERSION = "0.12.4";
+const CORE_VERSION = "0.12.5";
+const CORE_MT_VERSION = "0.12.5";
 
 const FFMPEG_TGZ = `ffmpeg-${FFMPEG_VERSION}.tgz`;
 const UTIL_TGZ = `util-${UTIL_VERSION}.tgz`;
