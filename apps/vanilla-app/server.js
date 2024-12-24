67cf76a1fae35050e58bbfbc1110b403d0ca1fce
Lucas Gelfond <lucas_gelfond@brown.edu>
2024-12-23 20:09:53 -0800

    set new server headers to debug GH actions

--- apps/vanilla-app/server.js
+++ apps/vanilla-app/server.js
@@ -6,9 +6,16 @@ const PORT = 8080;
 const ROOT = path.join(__dirname, "public");
 
 app.use((_, res, next) => {
-  res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
-  res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
-  res.setHeader("Cross-Origin-Resource-Policy", "cross-origin");
+  res.set({
+    "Cross-Origin-Opener-Policy": "same-origin",
+    "Cross-Origin-Embedder-Policy": "require-corp",
+    "Cross-Origin-Resource-Policy": "cross-origin",
+    "Origin-Agent-Cluster": "?1",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+    "Access-Control-Allow-Headers":
+      "Origin, X-Requested-With, Content-Type, Accept, Range",
+  });
   next();
 });
 
