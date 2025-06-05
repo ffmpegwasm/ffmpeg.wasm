c411a6b13c17349cbbe0be120240e7d29cb873e4
落日归山海 <951540966@qq.com>
2025-06-05 14:24:29 +0800

    Fix util umd entry

--- packages/util/webpack.config.cjs
+++ packages/util/webpack.config.cjs
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   mode: "production",
-  entry: "./dist/cjs/index.js",
+  entry: "./dist/esm/index.js",
   output: {
     library: "FFmpegUtil",
     libraryTarget: "umd",
